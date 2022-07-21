8eb24211370102535dc5c03f3772f85ae8ad8dae
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-07-21 16:16:24 +0200

    src/app/app-routing.module.ts Add routes of views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
 
-const routes: Routes = [];
+import {HomeComponent} from "./home/home.component";
+import {SignupComponent} from "./signup/signup.component";
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'signup', component: SignupComponent}
+  // {path: 'movies', component: MoviesComponent},
+  // {path: '', redirectTo: '/home', pathMatch: 'full'}
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
