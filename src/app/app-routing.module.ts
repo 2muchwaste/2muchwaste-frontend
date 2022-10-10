484d2fb74d1031ff376254837a3370a661762123
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-10-10 17:37:54 +0200

    Add routing to customer home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
 import {SignupComponent} from "./signup/signup.component";
 import {SigninComponent} from "./signin/signin.component";
+import {CustomerhomeComponent} from "./customerhome/customerhome.component";
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'signin', component: SigninComponent},
+  {path: 'customerhome', component: CustomerhomeComponent},
   // {path: 'movies', component: MoviesComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ]
