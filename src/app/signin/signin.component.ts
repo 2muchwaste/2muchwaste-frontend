2888f4bff5c2d364c5a99176c56a33eee1260831
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-10-10 08:25:43 +0200

    Create forms and receive which type of signin

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import {User} from "../models/user";
+import {map} from "rxjs/operators";
+import {AppConstants} from "../utils/constants";
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +12,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  type: string = ''
+  email: string = ''
+  password: string = ''
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,
+              private http: HttpClient) {  }
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.type = params['type'];
+      console.log("type="+this.type);
+    });
+
+  }
+
+  onSubmit(): void {
+    console.log("Pressed");
+    console.log("Mail inserita: " + this.email);
+    console.log("Password inserita: " + this.password);
+
+    // this.http.post(AppConstants.serverURL+'/api/v1/customers/auth/signup', this.user).pipe(map((responseData: any) => {
+    //   let dataObject: any = {}
+    //   dataObject = responseData;
+    //   return dataObject
+    // })).subscribe((response: any) => {
+    //   console.log("response: " +response)
+    //   //this.newMovie._id = response["_id"];
+    //   /*console.log("newmovie");
+    //   console.log(this.newMovie)
+    //   console.log("movie id");
+    //   console.log(response["_id"]);*/
+    // })
   }
 
 }
