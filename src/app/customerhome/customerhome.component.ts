397505b8977e516d0ef1c449e9ffa6613201dbe2
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-10-10 17:38:34 +0200

    Fill customer home with some generals data

--- src/app/customerhome/customerhome.component.ts
+++ src/app/customerhome/customerhome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../models/user";
 
 @Component({
   selector: 'app-customerhome',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customerhome.component.scss']
 })
 export class CustomerhomeComponent implements OnInit {
+
+  user = new User('Angelo', '', new Date(), '', '', 'Via mengoli 15'
+    , 61122, 'Pesaro', 'customer', '')
+
+  rubbishThrowing = 12
+  cumulativeDebt = 75
 
   constructor() { }
 
