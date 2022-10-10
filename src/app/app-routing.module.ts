a432243a13e0b456006d9af789e373c3b4cea00f
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-10-10 08:21:15 +0200

    Add to src/app/app-routing.module.ts signin path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import {RouterModule, Routes} from "@angular/router";
 
 import {HomeComponent} from "./home/home.component";
 import {SignupComponent} from "./signup/signup.component";
+import {SigninComponent} from "./signin/signin.component";
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'signup', component: SignupComponent},
+  {path: 'signin', component: SigninComponent},
   // {path: 'movies', component: MoviesComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ]
