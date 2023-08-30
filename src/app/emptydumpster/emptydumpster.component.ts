33debca436bfb3f97feb61d7ddec679449083e93
aletullio <74408746+aletullio@users.noreply.github.com>
2023-08-30 17:16:18 +0200

    emptydumpster

---
+++ src/app/emptydumpster/emptydumpster.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, Injectable  } from '@angular/core';
+
+export class Emptydumpster {
+  @Input() weight: number;
+
+  constructor(private recyclingService: RecyclingService) {}
+
+  emptydumpster() {
+    this.recyclingService.emptyBin().subscribe(() => {
+      this.weight = 0;
+    });
+  }
+}
+
+export class RecyclingService {
+
+private binWeight: number = 30; // Peso iniziale del bidone
+
+  emptyBin(): Observable<void> {
+  this.binWeight = 0;
+      return of();
+  }
+  
+  getBinWeight(): number {
+    return this.binWeight;
+  }
+}
