73f73d70d6d2504d09f28b203ff78c1f9804cfd3
Angelo Tinti <angelo.tinti.30@gmail.com>
2023-09-01 11:19:46 +0200

    Add check validation to signup field and fix hide visibility button bug

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {User} from "../models/user";
-import {map} from "rxjs/operators";
 import {AppConstants} from "../utils/constants";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {MatDatepicker} from "@angular/material/datepicker";
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
@@ -14,10 +12,10 @@ import {MatDatepicker} from "@angular/material/datepicker";
 
 export class SignupComponent implements OnInit {
 
-  // user = new User('', '', new Date(), '', '', 'Via mengoli 15'
-  //   , 61122, 'Pesaro', 'customer', '')
+  constructor(private http: HttpClient) {
+  }
 
-  roles: string[] = ['Admin', 'User', 'Operator']
+  roles: string[] = ['admin', 'user', 'operator']
 
   hide = true
 
@@ -32,11 +30,18 @@ export class SignupComponent implements OnInit {
   birthdayFormControl = new FormControl('', [Validators.required])
   roleFormControl = new FormControl('', [Validators.required])
 
-  formGroupt = [this.nameFormControl, this.surnameFormControl, this.emailFormControl, this.CFFormControl, this.passwordFormControl, this.zipCodeFormControl, this.cityFormControl, this.birthdayFormControl]
+  formGroup = [
+    this.nameFormControl,
+    this.surnameFormControl,
+    this.emailFormControl,
+    this.CFFormControl,
+    this.passwordFormControl,
+    this.zipCodeFormControl,
+    this.cityFormControl,
+    this.birthdayFormControl
+  ]
 
   // user = new User()
-  constructor(private http: HttpClient) {
-  }
 
   ngOnInit(): void {
   }
@@ -45,7 +50,11 @@ export class SignupComponent implements OnInit {
     return new User(
       this.nameFormControl.value,
       this.surnameFormControl.value,
-      ('' + this.birthdayFormControl.value.getFullYear() + '-' + this.birthdayFormControl.value.getMonth() + '-' + this.birthdayFormControl.value.getDate()),
+      (
+        this.birthdayFormControl.value.getFullYear() + '-'
+        + this.birthdayFormControl.value.getMonth() + '-'
+        + this.birthdayFormControl.value.getDate()
+      ),
       this.CFFormControl.value,
       this.emailFormControl.value,
       this.addressFormControl.value,
@@ -56,26 +65,36 @@ export class SignupComponent implements OnInit {
     )
   }
 
-  onSubmit(): void {
+  checkValidity(): boolean {
+    let valid = true
+    this.formGroup.forEach(form => {
+      if (valid && form.invalid) {
+        valid = false
+      }
+    })
+    return valid
+  }
+
+  checkAndSubmit() {
+    if (this.checkValidity()) {
+      this.submit()
+    }
+  }
+
+  submit(): void {
     console.log("Pressed")
     let user = this.initUser()
-    console.log(user)
-    this.http.post(AppConstants.serverURL + '/api/v1/auth/' + this.roleFormControl.value + '/signup', user).pipe(map((responseData: any) => {
-      let dataObject: any = {}
-      dataObject = responseData;
-      return dataObject
-    })).subscribe((response: any) => {
-      console.log("response: ")
-      console.log(response)
-      //this.newMovie._id = response["_id"];
-      /*console.log("newmovie");
-      console.log(this.newMovie)
-      console.log("movie id");
-      console.log(response["_id"]);*/
-    })
-    // console.log('Submitted')
-    // this.formGroupt.forEach((value) => console.log(value))
-    // console.log('User element:')
-    // console.log(this.initUser())
+    let requestUrl = AppConstants.serverURL + '/api/v1/auth/' + this.roleFormControl.value + '/signup'
+    this.http.post(requestUrl, user)
+      .subscribe({
+        next: (res) => {
+          console.log("Signup succes")
+          console.log(res)
+        },
+        error: (errorObject) => {
+          console.log("Signup error")
+          console.log(errorObject.error)
+        }
+      })
   }
 }
