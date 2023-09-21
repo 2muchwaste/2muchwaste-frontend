1ef9eda24cc219349c6454f69497cd0c2262dd5f
Angelo Tinti <angelo.tinti.30@gmail.com>
2023-09-21 18:21:51 +0200

    Add service to manage backend calls for user

---
+++ src/app/services/userservice.ts
@@ -0,0 +1,20 @@
+import {Injectable} from "@angular/core";
+import {AppConstants} from "../utils/constants";
+import {HttpRequestService} from "./httprequestservice";
+import {User} from "../models/user";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private backendUserURL = AppConstants.serverURL + AppConstants.versionBackend + 'users/'
+
+  constructor(
+    private httpReqService: HttpRequestService
+  ) {
+  }
+
+  getUsers(){
+    return this.httpReqService.getRequest<User[]>(this.backendUserURL)
+  }
+}
