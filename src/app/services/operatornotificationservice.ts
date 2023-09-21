e20a510804bbc845ed69854ba3f7cfcc7903e7e9
Angelo Tinti <angelo.tinti.30@gmail.com>
2023-09-21 18:11:32 +0200

    Add service to manage backend calls for operator notifications

---
+++ src/app/services/operatornotificationservice.ts
@@ -0,0 +1,78 @@
+import {Injectable} from "@angular/core";
+import {AppConstants} from "../utils/constants";
+import {HttpRequestService} from "./httprequestservice";
+import {DumpsterErrorType} from "../models/dumpstererrortype";
+import {OperatorNotificationStatus} from "../models/operatornotificationstatus";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OperatorNotificationService {
+  private backendOpNotificationURL = AppConstants.serverURL + AppConstants.versionBackend + 'opnotifications/'
+
+
+  constructor(
+    private httpReqService: HttpRequestService
+  ) {
+  }
+
+  getOperatorNotification() {
+    return this.httpReqService.getRequest(this.backendOpNotificationURL)
+  }
+
+  getPendingNotification() {
+    return this.httpReqService.getRequest(this.backendOpNotificationURL + 'status/pending')
+  }
+
+  getInProgressNotification() {
+    return this.httpReqService.getRequest(this.backendOpNotificationURL + 'status/inprogress')
+  }
+
+  getCompletedNotification() {
+    return this.httpReqService.getRequest(this.backendOpNotificationURL + 'status/complete')
+  }
+
+  getNotificationsByProblemFull() {
+    return this.httpReqService.getRequest(this.backendOpNotificationURL + NotificationErrorTypePath.FULL)
+  }
+
+  getNotificationsByProblemPhy() {
+    return this.httpReqService.getRequest(this.backendOpNotificationURL + NotificationErrorTypePath.PHYSICAL_PROBLEM)
+  }
+
+  getNotificationsByProblemObstruction() {
+    return this.httpReqService.getRequest(this.backendOpNotificationURL + NotificationErrorTypePath.OBSTRUCTION)
+  }
+
+  getNotificationsByProblemError() {
+    return this.httpReqService.getRequest(this.backendOpNotificationURL + NotificationErrorTypePath.ERROR)
+  }
+
+  createOperatorNotification(notificationType:string, dumpsterID:string) {
+    return this.httpReqService.postRequest(
+      this.backendOpNotificationURL,
+      {dumpsterID:dumpsterID,type:notificationType})
+  }
+
+  getNotificationByID(opNotificationID: string) {
+    return this.httpReqService.getRequest(this.backendOpNotificationURL + opNotificationID)
+  }
+
+  updateNotificationByID(opNotificationID:string, notificationStatus: OperatorNotificationStatus) {
+    return this.httpReqService.patchRequest(
+      this.backendOpNotificationURL + opNotificationID,
+      {status:notificationStatus})
+  }
+
+  deleteNotificationByID(opNotificationID:string) {
+    return this.httpReqService.deleteRequest(this.backendOpNotificationURL + opNotificationID)
+  }
+
+}
+
+enum NotificationErrorTypePath {
+  FULL='problems/full',
+  PHYSICAL_PROBLEM = 'problems/phy',
+  OBSTRUCTION ='problems/obstruction',
+  ERROR = 'problems/error',
+}
