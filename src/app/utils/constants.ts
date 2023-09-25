93e6c93a6ba0edb289fb2486dbd9a2550f0d90a2
Angelo Tinti <angelo.tinti.30@gmail.com>
2023-09-25 14:39:49 +0200

    Add constants for local storage key about token and userid

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,4 +1,6 @@
 export class AppConstants{
+  static localStorageUserID = "UserID"
+  static localStorageToken = 'token';
   public static get serverURL(): string { return "http://localhost:3456"}
   public static get versionBackend(): string {return "/api/v1/"}
 }
