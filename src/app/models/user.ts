27b696c4103119f2a89f2e4062d819899b672a9a
Angelo Tinti <angelo.tinti.30@gmail.com>
2023-09-25 13:43:09 +0200

    Add builder to user model

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -12,4 +12,74 @@ export class User {
     public password: string,
   ) {
   }
+
+}
+
+export class UserBuilder {
+  private user: any = {
+    name: '',
+    surname: '',
+    birthday: '',
+    cf: '',
+    email: '',
+    address: '',
+    zipCode: 0,
+    city: '',
+    role: '',
+    password: ''
+  };
+
+  setName(name: string): UserBuilder {
+    this.user.name = name;
+    return this;
+  }
+
+  setSurname(surname: string): UserBuilder {
+    this.user.surname = surname;
+    return this;
+  }
+
+  setBirthday(birthday: string): UserBuilder {
+    this.user.birthday = birthday;
+    return this;
+  }
+
+  setCF(cf: string): UserBuilder {
+    this.user.cf = cf;
+    return this;
+  }
+
+  setEmail(email: string): UserBuilder {
+    this.user.email = email;
+    return this;
+  }
+
+  setAddress(address: string): UserBuilder {
+    this.user.address = address;
+    return this;
+  }
+
+  setZipCode(zipCode: number): UserBuilder {
+    this.user.zipCode = zipCode;
+    return this;
+  }
+
+  setCity(city: string): UserBuilder {
+    this.user.city = city;
+    return this;
+  }
+
+  setRole(role: string): UserBuilder {
+    this.user.role = role;
+    return this;
+  }
+
+  setPassword(password: string): UserBuilder {
+    this.user.password = password;
+    return this;
+  }
+
+  build(): any {
+    return this.user;
+  }
 }
