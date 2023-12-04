880ef28d1d0defd5180a88039fd25843481af93d
Angelo Tinti <angelo.tinti.30@gmail.com>
2023-12-04 11:40:54 +0100

    Add PaymentStatus enum and PaymentsStatusManager to handle italians name

--- src/app/models/payment.ts
+++ src/app/models/payment.ts
@@ -6,5 +6,22 @@ export class Payment {
     public value: number,
     public status: string,
     public paymentDate: Date
-  ) {}
+  ) {
+  }
+}
+
+export enum PaymentStatus {
+  PENDING = 'pending',
+  COMPLETE = 'complete',
+}
+
+export class PaymentStatusManager{
+  private paymentStatus : Map<string,PaymentStatus> = new Map()
+  public static status : Map<PaymentStatus,{englishName: string,italianName: string}> = new Map()
+
+  constructor() {
+    PaymentStatusManager.status.set(PaymentStatus.PENDING, {englishName: PaymentStatus.PENDING,italianName:'Non contabilizzato'})
+    PaymentStatusManager.status.set(PaymentStatus.COMPLETE, {englishName: PaymentStatus.COMPLETE,italianName:'Completato'})
+    // this.paymentStatus.set(PaymentStatus.COMPLETE, {englishName: PaymentStatus.COMPLETE,italianName:'Completato'})
+  }
 }
