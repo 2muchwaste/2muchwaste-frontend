50be0e26d166cb625ef048e1986e777dfd674474
Angelo Tinti <angelo.tinti.30@gmail.com>
2023-12-04 11:42:29 +0100

    Modify id field name

--- src/app/models/signinresponse.ts
+++ src/app/models/signinresponse.ts
@@ -1,7 +1,7 @@
 export class SigninResponse {
   constructor(
     public email:string,
-    public _id:string,
+    public id:string,
     public role:{_id:string,name:string},
     public name:string,
     public token:string,
