ebb978b74ec30ee4927c1082311b1c8dbbb75e1a
Angelo Tinti <angelo.tinti.30@gmail.com>
2023-12-04 11:36:48 +0100

    Modify data type of trashtype using new model TrashType

--- src/app/models/deposit.ts
+++ src/app/models/deposit.ts
@@ -1,13 +1,14 @@
+import {TrashTypes} from "./trashtype";
+
 export class Deposit {
   constructor(
     public userID: string,
     public dumpsterID: string,
     public date: Date,
-    public type: string,
+    public type: TrashTypes,
     public quantity: number,
     public price: number,
     public openingTimeSeconds: number,
   ) {
-    // this.date = new Date(date)
   }
 }
