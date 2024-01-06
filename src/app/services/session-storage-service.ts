f4ebc934fc2c95897be4f540cd667ad93f5e0f1f
Angelo Tinti <angelo.tinti.30@gmail.com>
2024-01-06 13:22:19 +0100

    Change logo image in who we are component

--- src/app/services/session-storage-service.ts
+++ src/app/services/session-storage-service.ts
@@ -4,43 +4,43 @@ import {AppConstants} from "../utils/constants"
 @Injectable({
   providedIn: 'root',
 })
-export class LocalStorageService {
+export class SessionStorageService {
   private readonly USER_ID_KEY = AppConstants.lSUserID
   private readonly USER_TOKEN_KEY = AppConstants.lSToken
   private readonly USER_OBJECT_KEY = AppConstants.userObject
   private readonly USER_ROLE = AppConstants.lSuserRole
 
   setUserID(userID: string): void {
-    localStorage.setItem(this.USER_ID_KEY, userID)
+    sessionStorage.setItem(this.USER_ID_KEY, userID)
   }
 
   getUserID(): string | null {
-    return localStorage.getItem(this.USER_ID_KEY)
+    return sessionStorage.getItem(this.USER_ID_KEY)
   }
 
   setUserToken(userToken: string): void {
-    localStorage.setItem(this.USER_TOKEN_KEY, userToken)
+    sessionStorage.setItem(this.USER_TOKEN_KEY, userToken)
   }
 
   getUserToken(): string | null {
-    return localStorage.getItem(this.USER_TOKEN_KEY)
+    return sessionStorage.getItem(this.USER_TOKEN_KEY)
   }
 
   setUserObject(userObject: any): void {
     // Assuming userObject is a JSON-serializable object
     const serializedObject = JSON.stringify(userObject)
-    localStorage.setItem(this.USER_OBJECT_KEY, serializedObject)
+    sessionStorage.setItem(this.USER_OBJECT_KEY, serializedObject)
   }
 
   getUserObject(): any | null {
-    const serializedObject = localStorage.getItem(this.USER_OBJECT_KEY)
+    const serializedObject = sessionStorage.getItem(this.USER_OBJECT_KEY)
     return serializedObject ? JSON.parse(serializedObject) : null
   }
 
   clearLocalStorage(): void {
-    localStorage.removeItem(this.USER_ID_KEY)
-    localStorage.removeItem(this.USER_TOKEN_KEY)
-    localStorage.removeItem(this.USER_OBJECT_KEY)
-    localStorage.removeItem(this.USER_ROLE)
+    sessionStorage.removeItem(this.USER_ID_KEY)
+    sessionStorage.removeItem(this.USER_TOKEN_KEY)
+    sessionStorage.removeItem(this.USER_OBJECT_KEY)
+    sessionStorage.removeItem(this.USER_ROLE)
   }
 }
