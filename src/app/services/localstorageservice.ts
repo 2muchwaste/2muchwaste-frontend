e67e0f689cbb74aca58782b24a91724c61a645ca
Angelo Tinti <angelo.tinti.30@gmail.com>
2024-01-11 17:45:34 +0100

    Use localstorage instead sessionstorage

--- src/app/services/localstorageservice.ts
+++ src/app/services/localstorageservice.ts
@@ -11,36 +11,36 @@ export class LocalStorageService {
   private readonly USER_ROLE = AppConstants.lSuserRole
 
   setUserID(userID: string): void {
-    sessionStorage.setItem(this.USER_ID_KEY, userID)
+    localStorage.setItem(this.USER_ID_KEY, userID)
   }
 
   getUserID(): string | null {
-    return sessionStorage.getItem(this.USER_ID_KEY)
+    return localStorage.getItem(this.USER_ID_KEY)
   }
 
   setUserToken(userToken: string): void {
-    sessionStorage.setItem(this.USER_TOKEN_KEY, userToken)
+    localStorage.setItem(this.USER_TOKEN_KEY, userToken)
   }
 
   getUserToken(): string | null {
-    return sessionStorage.getItem(this.USER_TOKEN_KEY)
+    return localStorage.getItem(this.USER_TOKEN_KEY)
   }
 
   setUserObject(userObject: any): void {
     // Assuming userObject is a JSON-serializable object
     const serializedObject = JSON.stringify(userObject)
-    sessionStorage.setItem(this.USER_OBJECT_KEY, serializedObject)
+    localStorage.setItem(this.USER_OBJECT_KEY, serializedObject)
   }
 
   getUserObject(): any | null {
-    const serializedObject = sessionStorage.getItem(this.USER_OBJECT_KEY)
+    const serializedObject = localStorage.getItem(this.USER_OBJECT_KEY)
     return serializedObject ? JSON.parse(serializedObject) : null
   }
 
   clearLocalStorage(): void {
-    sessionStorage.removeItem(this.USER_ID_KEY)
-    sessionStorage.removeItem(this.USER_TOKEN_KEY)
-    sessionStorage.removeItem(this.USER_OBJECT_KEY)
-    sessionStorage.removeItem(this.USER_ROLE)
+    localStorage.removeItem(this.USER_ID_KEY)
+    localStorage.removeItem(this.USER_TOKEN_KEY)
+    localStorage.removeItem(this.USER_OBJECT_KEY)
+    localStorage.removeItem(this.USER_ROLE)
   }
 }
