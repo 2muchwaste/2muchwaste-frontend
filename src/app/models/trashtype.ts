a8031ae161b57cf02590ee8de013cac52c1b0fbb
Angelo Tinti <angelo.tinti.30@gmail.com>
2024-01-11 01:49:00 +0100

    Change trashtype colors

--- src/app/models/trashtype.ts
+++ src/app/models/trashtype.ts
@@ -16,15 +16,15 @@ export class TrashTypeManager {
 
   constructor() {
     this.typeToColorMap.set(TrashTypes.PAPER, {color: '#FF0000', italianName: 'Carta'}); // Red
-    this.typeToColorMap.set(TrashTypes.PLASTIC, {color: '#FFFF00', italianName: 'Plastica'}); // Yellow
+    this.typeToColorMap.set(TrashTypes.PLASTIC, {color: '#a1a127', italianName: 'Plastica'}); // Yellow
     this.typeToColorMap.set(TrashTypes.METAL, {color: '#FF00FF', italianName: 'Metallo'}); // Magenta
     this.typeToColorMap.set(TrashTypes.GLASS, {color: '#0000FF', italianName: 'Vetro'}); // Blue
     this.typeToColorMap.set(TrashTypes.GREEN, {color: '#008000', italianName: 'Verde'}); // Green
     this.typeToColorMap.set(TrashTypes.MEDICAL, {color: '#800080', italianName: 'Rifiuti Medici'}); // Purple
     this.typeToColorMap.set(TrashTypes.ELECTRICAL, {color: '#FFA500', italianName: 'Elettrico'}); // Orange
-    this.typeToColorMap.set(TrashTypes.OIL, {color: '#00FFFF', italianName: 'Olio'}); // Cyan
+    this.typeToColorMap.set(TrashTypes.OIL, {color: '#06c7c7', italianName: 'Olio'}); // Cyan
     this.typeToColorMap.set(TrashTypes.MIXED, {color: '#A0522D', italianName: 'Misto'}); // Sienna
-    this.typeToColorMap.set(TrashTypes.ORGANIC, {color: '#00FF00', italianName: 'Organico'}); // Lime Green
+    this.typeToColorMap.set(TrashTypes.ORGANIC, {color: '#88d73f', italianName: 'Organico'}); // Lime Green
 
 
   }
