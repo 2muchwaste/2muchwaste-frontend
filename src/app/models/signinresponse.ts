319d203526bbc6328a3991a2dd75ef68e6696a35
Angelo Tinti <angelo.tinti.30@gmail.com>
2024-01-12 01:55:21 +0100

    Reformat code

--- src/app/models/signinresponse.ts
+++ src/app/models/signinresponse.ts
@@ -1,10 +1,10 @@
-export class SigninResponse {
+export class SignInResponse {
   constructor(
-    public email:string,
-    public id:string,
-    public role:{_id:string,name:string},
-    public name:string,
-    public token:string,
+    public email: string,
+    public id: string,
+    public role: { _id: string, name: string },
+    public name: string,
+    public token: string,
   ) {
   }
 }
