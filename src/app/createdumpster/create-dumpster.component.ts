c1b6c87cf913b36795a3c503b5b6da2355d5feea
Angelo Tinti <angelo.tinti.30@gmail.com>
2024-02-02 16:43:40 +0100

    Add controls to add dumpster functionality

--- src/app/createdumpster/create-dumpster.component.ts
+++ src/app/createdumpster/create-dumpster.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
-import { MAT_DIALOG_DATA, MatDialog } from "@angular/material/dialog";
-import { Dumpster, DumpsterBuilder } from "../models/dumpster";
-import { DumpsterService } from "../services/backendcalls/dumpsterservice";
-import { TrashTypeManager } from "../models/trashtype";
-import { AuthenticationService } from "../services/backendcalls/authenticationservice";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, ValidatorFn, Validators} from "@angular/forms";
+import {MAT_DIALOG_DATA, MatDialog} from "@angular/material/dialog";
+import {Dumpster, DumpsterBuilder} from "../models/dumpster";
+import {DumpsterService} from "../services/backendcalls/dumpsterservice";
+import {TrashTypeManager, TrashTypes} from "../models/trashtype";
+import {AuthenticationService} from "../services/backendcalls/authenticationservice";
 import {CreateDumpsterService} from "../services/middleware/createdumpsterservice";
-import { Router } from "@angular/router";
-import { UserInformationService } from "../services/userinformationservice";
-import { OperatorInformationService } from "../services/operatorinformationservice";
-import { OperatorService } from '../services/backendcalls/operatorservice';
-import { LocalStorageService } from "../services/localstorageservice";
-import { RoleService } from "../services/backendcalls/roleservice";
+import {Router} from "@angular/router";
+import {UserInformationService} from "../services/userinformationservice";
+import {OperatorInformationService} from "../services/operatorinformationservice";
+import {OperatorService} from '../services/backendcalls/operatorservice';
+import {LocalStorageService} from "../services/localstorageservice";
+import {RoleService} from "../services/backendcalls/roleservice";
 import {DialogSimpleComponent} from "../dialogs/DialogSimple";
 
 @Component({
@@ -20,27 +20,30 @@ import {DialogSimpleComponent} from "../dialogs/DialogSimple";
   styleUrls: ['./../app.component.scss', './create-dumpster.component.scss']
 })
 export class CreateDumpsterComponent implements OnInit {
+  maxUsablePercentage = 90
+  minUsablePercentage = 10
   private readonly CLASS_TAG = "CreateDumpsterComponent";
   readonly trashTypeManager = new TrashTypeManager();
   dumpster: Dumpster;
   private createDumpsterForm!: FormGroup;
 
-  roles: string[] = [];
 
-  hide = true;
 
   actualWeightFormControl = new FormControl('', [Validators.required]);
   addressFormControl = new FormControl('', [Validators.required]);
   areaFormControl = new FormControl('', [Validators.required]);
-  availableFormControl = new FormControl('', [Validators.required]);
+  availableFormControl = new FormControl('', [Validators.required,this.availabilityValidator]);
   cityFormControl = new FormControl('', [Validators.required]);
   latitudineFormControl = new FormControl('', [Validators.required]);
   longitudineFormControl = new FormControl('', [Validators.required]);
-  limitUsablePercentageFormControl = new FormControl('', [Validators.required]);
+  // limitUsablePercentageFormControl = new FormControl('', [Validators.required,this.percentageValidator]);
+  limitUsablePercentageFormControl = new FormControl('', [Validators.required,Validators.min(this.minUsablePercentage),Validators.max(this.maxUsablePercentage)]);
   maxWeightFormControl = new FormControl('', [Validators.required]);
   openingSecondsDurationFormControl = new FormControl('', [Validators.required]);
   typeFormControl = new FormControl('', [Validators.required]);
   zipCodeFormControl = new FormControl('', [Validators.required]);
+
+  trashTypes: TrashTypes[];
 
   constructor(
     private dialog: MatDialog,
@@ -56,7 +59,9 @@ export class CreateDumpsterComponent implements OnInit {
     public roleService: RoleService,
   ) {
     this.dumpster = this.dumpsterService.dumpster;
-    
+
+    this.trashTypes = Object.values(TrashTypes)
+
     this.createDumpsterForm = this.fb.group({
       actualWeight: this.actualWeightFormControl,
       address: this.addressFormControl,
@@ -71,6 +76,11 @@ export class CreateDumpsterComponent implements OnInit {
       type: this.typeFormControl,
       zipCode: this.zipCodeFormControl
     });
+  }
+
+  availabilityValidator(control: FormControl): { [s: string]: boolean } {
+    const valid = /^(true|false)$/.test(control.value);
+    return valid ? {} : {"invalidAvailability": true}
   }
 
   ngOnInit(): void {
@@ -98,22 +108,22 @@ export class CreateDumpsterComponent implements OnInit {
       console.log("creazione bidone");
       const createObject = this.initDumpster()
       this.dumpsterService.createDumpster(createObject).subscribe({
-          next: (res) => {
-            console.log("bidone realizzato", res);
-            let dialogRef = this.dialog.open(DialogSimpleComponent,{
-              data: {
-                title: "Bidone creato",
-                message: "Bidone creato con successo"
-              }
-            })
-            dialogRef.afterClosed().subscribe({
-              next:(res)=>{
-                this.router.navigate(['/operatorhome'])
-              }
-            })
-         
-          }
-        });
+        next: (res) => {
+          console.log("bidone realizzato", res);
+          let dialogRef = this.dialog.open(DialogSimpleComponent, {
+            data: {
+              title: "Bidone creato",
+              message: "Bidone creato con successo"
+            }
+          })
+          dialogRef.afterClosed().subscribe({
+            next: (res) => {
+              this.router.navigate(['/operatorhome'])
+            }
+          })
+
+        }
+      });
     }
   }
-} 
+}
